f06140158fe31d7d6ade4dd36911ed1780e7e163
Pham Vu Phuong <chitraenco@yahoo.com>
2025-08-25 17:46:11 +0700

    sửa

--- main.tf
+++ main.tf
@@ -126,8 +126,7 @@ resource "aws_route_table" "private-route-a" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat-gw-a.id
- Implementing the Infrastructure | 153
+    nat_gateway_id = aws_nat_gateway.nat-gw-a.id 
   }
   tags = {
     "Name" = "${local.vpc_name}-private-route-a"
